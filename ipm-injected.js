9de7ece4ff0bde976108ddf86ccbec1f11a68b74
Gabriel Birke <birke@d-scribe.de>
2013-05-30 23:43:10 +0200

    Initial commit

---
+++ ipm-injected.js
@@ -0,0 +1,22 @@
+document.addEventListener("contextmenu", handleMessage, false);
+
+function handleMessage(evt) {
+	var userInfo, link = evt.target;;
+	if(link.nodeType == 3) {
+		link = link.parentNode;
+	}
+	if(link.nodeType != 1) {
+		return;
+	}
+	
+	while(link != null && link.nodeName.toLowerCase() != "a") {
+		link = link.parentNode;
+	}
+	if(link && link.href && link.href.match(/^http/)) {
+		userInfo = { type: "link", url: link.href }
+	}
+	else {
+		userInfo = { type: "page", url: window.location.href }
+	}
+    safari.self.tab.setContextMenuEventUserInfo(evt, userInfo);
+}
